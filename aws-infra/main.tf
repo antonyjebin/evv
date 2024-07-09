a34428f29d0c7817ba8df9e503e55de681015ad1
Antony Jebinraj Y <125467398+antonyjebin@users.noreply.github.com>
2024-07-09 20:43:56 +0530

    Update main.tf

--- aws-infra/main.tf
+++ aws-infra/main.tf
@@ -2,12 +2,3 @@ provider "aws" {
   region = "us-east-1"
 }
 data "aws_availability_zones" "available" {}
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-bucket"
-  acl    = "private"
-  
-  versioning {
-    enabled = true
-  }
-}
