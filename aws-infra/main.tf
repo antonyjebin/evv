0f77fd413c2ed449fb438f6236e203b32deac153
jebin <jebin1848@gmail.com>
2024-07-09 19:43:48 +0530

    add s3 files

--- aws-infra/main.tf
+++ aws-infra/main.tf
@@ -2,3 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 data "aws_availability_zones" "available" {}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state-bucket"
+  acl    = "private"
+  
+  versioning {
+    enabled = true
+  }
+}
