20585375d95abc8d4cc677dca426593a9215c85a
Antony Jebinraj Y <125467398+antonyjebin@users.noreply.github.com>
2024-07-09 20:49:51 +0530

    Update ec2.tf

--- aws-infra/ec2.tf
+++ aws-infra/ec2.tf
@@ -1,32 +1,21 @@
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-
 resource "aws_instance" "app" {
   count                     = 2
-  ami                       = data.aws_ami.amazon_linux.id
+  ami                       = "ami-0c2af51e265bd5e0e"
   instance_type             = "t2.micro"
   subnet_id                 = element(aws_subnet.private.*.id, count.index)
   security_groups           = [aws_security_group.app_sg.id]
-  associate_public_ip_address = true
+  associate_public_ip_address = false
 
   tags = {
     Name = "AppInstance${count.index}"
   }
 
-
   user_data = <<-EOF
     #!/bin/bash
-    yum update -y
-    yum install -y docker
-    service docker start
-    chkconfig docker on
+    apt-get update
+    apt-get install -y docker.io
+    systemctl start docker
+    systemctl enable docker
     docker run -d -p 3000:3000 antonyjebinraj/my-app
   EOF
 }
